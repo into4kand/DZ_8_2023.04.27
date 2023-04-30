3cd421c2759c39e5ac79f3434a52b5173f730bc3
Inna Glebova <into4kand@mail.ru>
2023-04-30 14:19:28 +0300

    Second Ex done

---
+++ Ex2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Введите трехзначное число ");
+int number = Convert.ToInt32(Console.ReadLine());
+while (number > 999)
+    {
+        number /=10;
+    }
+if (number > 99)
+{
+    int a = number % 10;
+    Console.WriteLine($"Третья цифра в числе это {a}");
+}
+else
+{
+    Console.WriteLine("В числе нет третьей цифры ");
+}
