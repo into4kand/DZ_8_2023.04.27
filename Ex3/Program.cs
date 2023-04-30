7acce0995a85f79ad26a8b4d6c36d69c192a29cd
Inna Glebova <into4kand@mail.ru>
2023-04-30 14:00:00 +0300

    Third Ex done

---
+++ Ex3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным
+Console.WriteLine("Введите число ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a < 1 || a > 7)
+{
+    Console.WriteLine("Try again ");
+}
+else if (a > 0 & a < 6)
+{
+    Console.WriteLine("No");
+}
+else
+{
+    Console.WriteLine("Yes");
+}
